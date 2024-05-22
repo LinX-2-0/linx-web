806fa3fb9e5eaa4b27a1aa00060fcbdb7631d2fb
Kabil Hussain <68674204+kabil166@users.noreply.github.com>
2024-05-23 00:16:43 +0530

    Update api.js

--- src/middliware/api.js
+++ src/middliware/api.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-    hostname: 'http://localhost:5000',
+    hostname: 'http://13.201.36.2:5000',
     endpoints: {
         login: 'api/login',
         getUserList: '/api/users',
